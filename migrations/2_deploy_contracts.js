82d0c414c9239924f5290f22c3cec7b2716ba373
Tim Alander <tim.alander@gmail.com>
2018-03-04 19:38:30 -0500

    Cleaning up formatting on JS files

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,21 +1,19 @@
 const SGTCoin = artifacts.require('SGTCoin');
 const SGTCoinCrowdsale = artifacts.require('SGTCoinCrowdsale');
 
-const ether = (n) => {
-  return new web3.BigNumber(web3.toWei(n, 'ether'));
-}
+const ether = (n) => new web3.BigNumber(web3.toWei(n, 'ether'));
 
 module.exports = (deployer) => {
-  const tokenCap = ether(100000000);
-  deployer.deploy(SGTCoin, tokenCap)
-  .then(() => {
-    const rate = new web3.BigNumber(800);
-    const crowdsaleCap = ether(50000000);
-    const walletAddress = '0x5AEDA56215b167893e80B4fE645BA6d5Bab767DE';
-    return deployer.deploy(SGTCoinCrowdsale, rate, walletAddress, SGTCoin.address, crowdsaleCap);
-  }).then(() => {
-    return SGTCoin.deployed();
-  }).then((tokenInstance) => {
-    return tokenInstance.transferOwnership(SGTCoinCrowdsale.address);
-  });
+    const tokenCap = ether(100000000);
+    deployer.deploy(SGTCoin, tokenCap)
+    .then(() => {
+        const rate = new web3.BigNumber(800);
+        const crowdsaleCap = ether(50000000);
+        const walletAddress = '0x5AEDA56215b167893e80B4fE645BA6d5Bab767DE';
+        return deployer.deploy(SGTCoinCrowdsale, rate, walletAddress, SGTCoin.address, crowdsaleCap);
+    }).then(() => {
+        return SGTCoin.deployed();
+    }).then((tokenInstance) => {
+        return tokenInstance.transferOwnership(SGTCoinCrowdsale.address);
+    });
 };
--- test/Crowdsale.test.js
+++ test/Crowdsale.test.js
@@ -1,49 +1,46 @@
 const Token = artifacts.require('SGTCoin');
 const Crowdsale = artifacts.require('SGTCoinCrowdsale');
 
-const ether = (n) => {
-  return new web3.BigNumber(web3.toWei(n, 'ether'));
-}
+const ether = (n) => new web3.BigNumber(web3.toWei(n, 'ether'));
 
 contract('SGT Token Crowdsale', (accounts) => {
-  it('only allow contract owner to access whitelisting functions', async () => {
-    const crowdsale = await Crowdsale.deployed();
+    it('only allow contract owner to access whitelisting functions', async () => {
+        const crowdsale = await Crowdsale.deployed();
 
-    // False cases: assert non-owner cannot modify whitelist
-    try {
-      await crowdsale.addToWhitelist(accounts[1], {from: accounts[1]});
-    } catch (e) {} finally {
-      const resultA = await crowdsale.whitelist.call(accounts[1]);
-      assert.equal(resultA, false);
-    }
+        // False cases: assert non-owner cannot modify whitelist
+        try {
+            await crowdsale.addToWhitelist(accounts[1], {from: accounts[1]});
+        } catch (e) {} finally {
+            const resultA = await crowdsale.whitelist.call(accounts[1]);
+            assert.equal(resultA, false);
+        }
 
-    try {
-      await crowdsale.addToWhitelist(accounts[1], {from: accounts[0]});
-      await crowdsale.removeFromWhitelist(accounts[1], {from: accounts[1]});
-    } catch (e) {} finally {
-      const resultB = await crowdsale.whitelist.call(accounts[1]);
-      assert.equal(resultB, true);
-    }
+        try {
+            await crowdsale.addToWhitelist(accounts[1], {from: accounts[0]});
+            await crowdsale.removeFromWhitelist(accounts[1], {from: accounts[1]});
+        } catch (e) {} finally {
+            const resultB = await crowdsale.whitelist.call(accounts[1]);
+            assert.equal(resultB, true);
+        }
 
-    // True cases: assert owner can modify whitelist
-    await crowdsale.addToWhitelist(accounts[2], {from: accounts[0]});
-    const resultC = await crowdsale.whitelist.call(accounts[2]);
-    assert.equal(resultC, true);
+        // True cases: assert owner can modify whitelist
+        await crowdsale.addToWhitelist(accounts[2], {from: accounts[0]});
+        const resultC = await crowdsale.whitelist.call(accounts[2]);
+        assert.equal(resultC, true);
 
-    await crowdsale.removeFromWhitelist(accounts[2], {from: accounts[0]});
-    const resultD = await crowdsale.whitelist.call(accounts[2]);
-    assert.equal(resultD, false);
-  });
+        await crowdsale.removeFromWhitelist(accounts[2], {from: accounts[0]});
+        const resultD = await crowdsale.whitelist.call(accounts[2]);
+        assert.equal(resultD, false);
+    });
 
-  it('only whitelisted addresses can buy tokens', async () => {
-    const crowdsale = await Crowdsale.deployed();
-    const token = await Token.deployed();
+    it('only whitelisted addresses can buy tokens', async () => {
+        const crowdsale = await Crowdsale.deployed();
+        const token = await Token.deployed();
 
-    await crowdsale.addToWhitelist(accounts[2], {from: accounts[0]});
+        await crowdsale.addToWhitelist(accounts[2], {from: accounts[0]});
+        await crowdsale.sendTransaction({from: accounts[2], value: ether(1)});
 
-    await crowdsale.sendTransaction({from: accounts[2], value: ether(1)});
-
-    const balance = await token.balanceOf.call(accounts[2]);
-    assert.equal(balance.toString(10), ether(800).toString(10));
-  });
+        const balance = await token.balanceOf.call(accounts[2]);
+        assert.equal(balance.toString(10), ether(800).toString(10));
+    });
 });
